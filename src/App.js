2f59c05b6a4f34bdf85b2ad2e2a68173b39f3fe6
Alexander Fok <foktmalexander@gmail.com>
2021-04-02 08:24:29 -0700

    Pass props for Filter Component. Add function for handling filter. Add new state 'all' for storing all employee info feteched from API

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Row from 'react-bootstrap/Row';
 import EmployeeTable from './components/EmployeeTable';
 import SortOptions from './components/SortOptions';
 import FilterInput from './components/FilterInput';
-import './App.css';
 
 const App = () => {
+  // variable 'all' stores all employees fetched from API
+  const [all, setAll] = useState([]);
+  // variable 'employees' represents the employees being displayed
   const [employees, setEmployees] = useState([]);
+
   const [sortCategory, setSortCat] = useState();
   const [sortOrder, setSortOrder] = useState('ascend');
-  const [filterCategory, setFilterCat] = useState();
-  const [filteWord, setFilterWord] = useState("");
+  const [filterWord, setFilterWord] = useState('');
+  const [filterCategory, setFilterCat] = useState('name');
 
   useEffect(() => {
-    // execute anonymus async function for getting employees
+    // execute anonymus async function for getting all employees
     (async() => {
       const { data } = (await getEmployeeAPI());
       
-      if (data.error)
-        return console.log(data.error);
-
+      if (data.error) return console.log(data.error);
+      setAll(data.results);
       setEmployees(data.results);
     })();
   },[]);
@@ -40,6 +42,14 @@ const App = () => {
     return employees.slice().sort(sortByCat[category][order]);
   }
 
+  const filterEmployees = (employees, keyword, category) => {
+    const filter = {
+      'name': e => e.name.first.toLowerCase().includes(keyword, 0),
+      'username': e => e.login.username.toLowerCase().includes(keyword, 0)
+    }
+    return employees.filter(filter[category])
+  }
+
   const handleSort = (category) => {
     if (category === sortCategory) return;
 
@@ -53,14 +63,26 @@ const App = () => {
     setSortOrder(order);
     sortCategory
       ? setEmployees(sortEmployees(sortCategory, order))
-      : setEmployees(e => e.reverse());
+      : setEmployees(prev => prev.reverse());
   }
 
-  const handleFilterCategory = () => {
+  const handleFilterWord = (input) => {
+    const word = input.trim().toLowerCase();
+    setFilterWord(word.toLowerCase());
+
+    word.length > 2 && filterCategory
+      ? setEmployees(prev => filterEmployees(prev, word, filterCategory))
+      : setEmployees(all);
+  }
+
+  const handleFilterCategory = (category) => {
+    if (category === filterCategory) return;
+
+    setFilterCat(category);
     
+    if (filterWord.length > 2)
+      setEmployees(prev => filterEmployees(prev, filterWord, category));
   }
-
-  const handleFilterWord = (word) => setFilterWord(word)
 
   return (
     <Container>
@@ -71,7 +93,12 @@ const App = () => {
           handleSort={handleSort}
           handleOrder={handleOrder}
         />
-        <FilterInput />
+        <FilterInput
+          filterWord={filterWord}
+          filterCategory={filterCategory}
+          handleFilterWord={handleFilterWord}
+          handleFilterCategory={handleFilterCategory}
+        />
       </Row>
       <Row>
         <EmployeeTable employees={employees}/>
