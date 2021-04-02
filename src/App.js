ce6a476fc1850d4835bc835c18626c43fcdc8399
Alexander Fok <foktmalexander@gmail.com>
2021-04-01 17:20:08 -0700

    Add container layout, sorting related functions and states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react';
 import getEmployeeAPI from './utils/getEmployeesAPI';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import EmployeeTable from './components/EmployeeTable';
+import SortOptions from './components/SortOptions';
+import './App.css';
 
 const App = () => {
   const [employees, setEmployees] = useState([]);
+  const [sortCategory, setSortCat] = useState();
+  const [sortOrder, setSortOrder] = useState('ascend');
 
   useEffect(() => {
+    // execute anonymus async function for getting employees
     (async() => {
       const { data } = (await getEmployeeAPI());
       
@@ -16,8 +23,47 @@ const App = () => {
     })();
   },[]);
 
+  const sortEmployees = (category, order = sortOrder) => {
+    const sortByCat = {
+      "name": (a, b) =>
+        order === 'ascend'
+          ? a.name.first.localeCompare(b.name.first)
+          : b.name.first.localeCompare(a.name.first)
+      ,
+      "username": (a, b) =>
+        order === 'ascend'
+          ? a.login.username.localeCompare(b.login.username)
+          : b.login.username.localeCompare(a.login.username)
+    }
+    setSortCat(category);
+    setEmployees(employees.slice().sort(sortByCat[category]));
+  }
+
+  const handleSort = (category) => sortEmployees(category);    
+
+  const handleOrder = (selection) => {
+    console.log(`selection: ${selection}\nsortOrder: ${sortOrder}`);
+    if (selection !== sortOrder) {
+      console.log(`sortCategory: ${sortCategory}\nselection: ${selection}\nsortOrder: ${sortOrder}`);
+      setSortOrder(selection);
+      if (sortCategory) sortEmployees(sortCategory, selection);
+    }
+  }
+
   return (
-    <EmployeeTable employees={employees}/>
+    <Container>
+      <Row>
+        <SortOptions
+          sortCategory={sortCategory}
+          handleSort={handleSort}
+          sortOrder={sortOrder}
+          handleOrder={handleOrder}
+        />
+      </Row>
+      <Row>
+        <EmployeeTable employees={employees}/>
+      </Row>
+    </Container>
   )
 }
 export default App;
